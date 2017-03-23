d8704081c24f9331ff635ac1cfc5220b0949c565
Oh Daniel <Oh Daniel>
2017-03-23 10:05:42 -0400

    Updated GUI

--- src/Display.java
+++ src/Display.java
@@ -7,14 +7,18 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
+import sun.java2d.pipe.SpanShapeRenderer;
 
+import javax.swing.*;
 import java.io.File;
+import java.text.SimpleDateFormat;
 
 /**
  * Created by Daniel Oh on 3/22/2017.
@@ -24,6 +28,8 @@ public class Display extends Application{
 
     Stage mainStage;
     Scene Login_Scene, Forgot_Password_Scene;
+    File file = new File(".");
+    Image Logo = new Image("ABIC_Logo.png");
 
     public static void main(String[] args) {
         launch(args);
@@ -45,10 +51,9 @@ public class Display extends Application{
         grid.setHgap(5);
         grid.setVgap(5);
         grid.setPadding(new Insets(10,10,20,20));
+        grid.setStyle("-fx-background-color: white");
 
         //Row 1 (Logo)
-        File file = new File(".");
-        Image Logo = new Image("ABIC_Logo.png");
         ImageView LogoView = new ImageView();
         LogoView.setImage(Logo);
         LogoView.setFitWidth(Logo.getWidth() / 4);
@@ -92,7 +97,9 @@ public class Display extends Application{
                 mainStage.setScene(Forgot_Password_Scene);
             }
         });
+
         Login_Scene = new Scene(grid, 800,600);
+
     }
 
     public void Forgot_Password_Call(){
@@ -101,17 +108,26 @@ public class Display extends Application{
         grid.setHgap(5);
         grid.setVgap(5);
         grid.setPadding(new Insets(10,10,20,20));
+        grid.setStyle("-fx-background-color: white");
+
+        //Row 0 (Logo)
+        ImageView LogoView = new ImageView();
+        LogoView.setImage(Logo);
+        LogoView.setFitWidth(Logo.getWidth()/4);
+        LogoView.setFitHeight(Logo.getHeight()/4);
+        grid.add(LogoView,6,0,3,1);
+
 
         //Row 1
         Label Forgot_Password_Label = new Label("Forgot Password");
         Forgot_Password_Label.setFont(Font.font("Tahoma", FontWeight.BOLD, 14));
-        grid.add(Forgot_Password_Label,0,0,2,1);
+        grid.add(Forgot_Password_Label,0,1,2,1);
         Forgot_Password_Scene = new Scene(grid,800,600);
 
         //Row 2
         Label reset_password_info = new Label("Reset Password through:");
         reset_password_info.setFont(Font.font("Calibri", FontWeight.NORMAL, 11));
-        grid.add(reset_password_info,0,1,2,1);
+        grid.add(reset_password_info,0,2,2,1);
 
         //Row 3
         Label email_label = new Label();
@@ -119,8 +135,33 @@ public class Display extends Application{
         email_label.setFont(Font.font("Calibri", FontWeight.NORMAL, 11));
         TextField email_Field = new TextField();
         email_Field.setText("Enter email");
-        grid.add(email_label, 1,2,1,1);
-        grid.add(email_Field, 2,2,2,1);
+        grid.add(email_label, 1,3,1,1);
+        grid.add(email_Field, 2,3,2,1);
+
+        //Row 4
+        Label mobile_label = new Label();
+        mobile_label.setText("○ Mobile number:");
+        mobile_label.setFont(Font.font("Calibri", FontWeight.NORMAL, 11));
+        TextField mobile_Field = new TextField();
+        mobile_Field.setText("Enter mobile number");
+        grid.add(mobile_label, 1,4,1,1);
+        grid.add(mobile_Field,2,4,2,1);
+
+        //Row 5
+        Label empty_label = new Label(" ");
+        grid.add(empty_label,0,5,1,1);
+
+        //Row 6
+        Label ssn_Label = new Label();
+        ssn_Label.setText("Please verify your Social Security Number: ");
+        ssn_Label.setFont(Font.font("Calibri", FontWeight.NORMAL, 11));
+        TextField ssn_Field = new TextField();
         
+        JFormattedTextField ssn_format = new JFormattedTextField();
+
+        grid.add(ssn_Label, 0,6,2,1);
+
+
+
     }
 }
