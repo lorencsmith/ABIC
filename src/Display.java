b56dbd00689ccc9e16e1a3a0009d246ba1f3cd83
Oh Daniel <Oh Daniel>
2017-03-27 22:22:23 -0400

    Updated Enroll Page

--- src/Display.java
+++ src/Display.java
@@ -7,23 +7,17 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.control.TextFormatter;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import sun.java2d.pipe.SpanShapeRenderer;
 
-import javax.swing.*;
-import javax.swing.text.MaskFormatter;
-import java.awt.*;
 import java.io.File;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.logging.SimpleFormatter;
 
 /**
  * Created by Daniel Oh on 3/22/2017.
@@ -32,7 +26,7 @@ import java.util.logging.SimpleFormatter;
 public class Display extends Application{
 
     Stage mainStage;
-    Scene Login_Scene, Forgot_Password_Scene;
+    Scene Login_Scene, Forgot_Password_Scene, Enroll_Scene;
     File file = new File(".");
     Image Logo = new Image("ABIC_Logo.png");
 
@@ -88,6 +82,14 @@ public class Display extends Application{
         Enroll_Button.setFont(Font.font("Tahoma", FontWeight.BOLD, 12));
         Enroll_Button.setMinWidth(70);
         grid.add(Enroll_Button, 0, 4, 1,1);
+
+        Enroll_Button.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                Enroll_Call();
+            }
+        });
+
         Button Forgot_Button = new Button("Forgot Password");
         Forgot_Button.setAlignment(Pos.CENTER);
         Forgot_Button.setPadding(new Insets(5,5,5,5));
@@ -99,10 +101,8 @@ public class Display extends Application{
             @Override
             public void handle(ActionEvent event) {
                 Forgot_Password_Call();
-                mainStage.setScene(Forgot_Password_Scene);
             }
         });
-
         Login_Scene = new Scene(grid, 800,600);
 
     }
@@ -122,12 +122,11 @@ public class Display extends Application{
         LogoView.setFitHeight(Logo.getHeight()/4);
         grid.add(LogoView,6,0,3,1);
 
-
         //Row 1
         Label Forgot_Password_Label = new Label("Forgot Password");
         Forgot_Password_Label.setFont(Font.font("Tahoma", FontWeight.BOLD, 14));
         grid.add(Forgot_Password_Label,0,1,2,1);
-        Forgot_Password_Scene = new Scene(grid,800,600);
+
 
         //Row 2
         Label reset_password_info = new Label("Reset Password through:");
@@ -180,6 +179,83 @@ public class Display extends Application{
         button_Box.getChildren().addAll(cancel_Button,submit_Button);
         grid.add(button_Box, 0,7,3,1);
 
+        Forgot_Password_Scene = new Scene(grid, 800,600);
+        mainStage.setScene(Forgot_Password_Scene);
+    }
 
+    public void Enroll_Call(){
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setHgap(5);
+        grid.setVgap(5);
+        grid.setPadding(new Insets(10,10,20,20));
+        grid.setStyle("-fx-background-color: white");
+
+        //Row 0
+        ImageView LogoView = new ImageView();
+        LogoView.setImage(Logo);
+        LogoView.setFitWidth(Logo.getWidth()/4);
+        LogoView.setFitHeight(Logo.getHeight()/4);
+        grid.add(LogoView,6,0,3,1);
+
+        //Row 1
+        Label Welcome_Logo = new Label();
+        Welcome_Logo.setText("Welcome to ABIC Banking Enrollment");
+        Welcome_Logo.setFont(Font.font("Calibri", FontWeight.BOLD, 13));
+        grid.add(Welcome_Logo,0,1,3,1);
+
+        //Row 2
+        Label Info_Logo = new Label();
+        Info_Logo.setText("Please enter your information in the spaces provided below");
+        Info_Logo.setFont(Font.font("Calibri", FontWeight.BOLD, 12));
+        grid.add(Info_Logo,0,2,3,1);
+
+        //Row 3
+        HBox First_Name_Box = new HBox();
+        First_Name_Box.setAlignment(Pos.CENTER);
+        Label First_Name_Label = new Label();
+        First_Name_Label.setText("First Name");
+        First_Name_Label.setFont(Font.font("",FontWeight.BOLD, 11));
+        First_Name_Box.getChildren().addAll(First_Name_Label,red_star());
+        TextField First_Name_Field = new TextField();
+        First_Name_Field.setPromptText("Enter First Name");
+        grid.add(First_Name_Box,0,3,1,1);
+        grid.add(First_Name_Field, 1, 3,1,1);
+
+        //Row 4
+        Label First_Name_Info = new Label();
+        First_Name_Info.setText("Enter your first name as it appears on your primary billing account.");
+        First_Name_Info.setFont(Font.font("",FontWeight.NORMAL, 11));
+        grid.add(First_Name_Info, 0,4,3,1);
+
+        //Row 5
+        HBox Last_Name_Box = new HBox();
+        Last_Name_Box.setAlignment(Pos.CENTER);
+        Label Last_Name_Label = new Label();
+        Last_Name_Label.setText("Last Name");
+        Last_Name_Label.setFont(Font.font("",FontWeight.BOLD, 11));
+        Last_Name_Box.getChildren().addAll(Last_Name_Label,red_star());
+        TextField Last_Name_Field = new TextField();
+        Last_Name_Field.setPromptText("Enter Last Name");
+        grid.add(Last_Name_Box,0,5,1,1);
+        grid.add(Last_Name_Field, 1, 5,1,1);
+
+        //Row 6
+        Label Last_Name_Info = new Label();
+        Last_Name_Info.setText("Enter your last name as it appears on your primary billing account.");
+        Last_Name_Info.setFont(Font.font("",FontWeight.NORMAL, 11));
+        grid.add(Last_Name_Info, 0,6,3,1);
+
+
+        Enroll_Scene = new Scene(grid, 800,600);
+        mainStage.setScene(Enroll_Scene);
+    }
+
+    public Text red_star(){
+        //Red Star
+        Text rs = new Text();
+        rs.setText("*");
+        rs.setFill(Color.RED);
+        return rs;
     }
 }
