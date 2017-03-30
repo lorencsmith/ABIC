b43a5308859df2235d38c284d9d19be06e7de164
lorencsmith <lorencsmith@comcast.net>
2017-03-30 15:17:00 -0400

    Add files via upload
    AccountAction skeleton

---
+++ interfaces/AccountAction.java
@@ -0,0 +1,42 @@
+import java.util.HashMap;
+
+/** The AccountAction class contains the methods for the accounts, including
+ *  the withdraw, deposit.
+ */
+public class AccountAction {
+    
+    /**
+     * Variable for the total available balance of the account.
+     * @invariant availableBalance !< 0
+     */
+    private double availableBalance;
+    
+    
+    /** 
+     * The withdraw() method is a method to subtract funds from the checking account.
+     *
+     */
+    
+    public void withdraw(double withdrawAmt){
+        
+    }
+    
+    /**
+     * The deposit() method is used to add funds to the checking or savings account.
+     *
+     */
+    public void deposit(double depositAmt){
+        
+    }
+    
+    /**
+     * The transfer() method is used to take funds from one account, and place the funds in a different account.
+     * The transfer can be used between checking and/or savings accounts.
+     */
+    
+    public void transfer(double transferAmt){
+        
+    }
+    
+    
+}
