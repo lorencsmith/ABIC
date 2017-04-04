c1c228905304da6f882a01968548f02dedf85d2d
Grandy <deadfisheyes1@gmail.com>
2017-04-04 18:00:56 -0400

    Implemented some BankAccount class functions(working on fixing SQLITE_BUSY Error

---
+++ src/BankAccountDemo.java
@@ -0,0 +1,89 @@
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Calendar;
+import java.util.Random;
+
+/**
+ * Created by Lunanamic on 4/4/2017.
+ */
+public class BankAccountDemo {
+    public static void main(String[] args) {
+
+        deleteDatabase("./Database Files/", "Main.db");
+
+        oneTimeSetup("Main.db");
+
+        initialTableSetup();
+
+        BankAccountDriver test = new BankAccountDriver();
+
+        System.out.println("Withdrawing $100");
+        test.withhdraw(100.0, 717, 101);
+        DatabaseDriver.viewTable("Account");
+
+
+    }
+
+    public static void deleteDatabase(String filepath, String dbName) {
+        File f = null;
+        boolean deleted = false;
+
+        try {
+
+            String path = filepath + dbName;
+
+            f = new File(path);
+
+            deleted = f.delete();
+
+            if (deleted)
+                System.out.println("Database deleted successfully");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void oneTimeSetup(String dbName) {
+        DatabaseDriver.createNewDatabase(dbName);
+        DatabaseDriver.createNewTable(DatabaseDriver.createLocalAccount());
+        DatabaseDriver.createNewTable(DatabaseDriver.createPerson());
+        DatabaseDriver.createNewTable(DatabaseDriver.createAccount());
+    }
+
+    public static void initialTableSetup() {
+
+        Random rand = new Random();
+
+        Calendar calendar = Calendar.getInstance();
+
+        java.sql.Timestamp currentTimestamp = new java.sql.Timestamp(calendar.getTime().getTime());
+
+        String sql = String.format("INSERT INTO 'LOCAL ACCOUNT'(Pk_LocalAccount_Id, USERNAME, PASSWORD)" +
+                "VALUES(\"100\", \"Admin\", \"pass\")");
+        DatabaseDriver.run(sql);
+
+        sql = "INSERT INTO Person ('Pk_Person_Id','FIRST NAME', 'LAST NAME', SSN, DOB, 'ADDRESS', CITY, STATE, 'ZIP CODE', 'HOME NUMBER', 'WORK NUMBER')" +
+                "VALUES ((SELECT Pk_LocalAccount_Id FROM 'LOCAL ACCOUNT' WHERE USERNAME = \"Admin\" limit 1), \"1\", \"2\", \"2\", \"2\", \"2\", \"2\", \"2\", \"2\", \"2\", \"2\")";
+        DatabaseDriver.run(sql);
+
+        sql = sql = String.format("INSERT INTO Account ('Pk_Account_Id', Account_Number, Type, Balance, Interest_Rate, Overdraft, Last_Access_Time_Stamp)" +
+                "VALUES (\"717\", \"101\", \"1\", \"500.14\", \"0\", \"0.00\", \"%s\")", currentTimestamp);
+        DatabaseDriver.run(sql);
+
+
+        sql = sql = String.format("INSERT INTO Account ('Pk_Account_Id', Account_Number, Type, Balance, Interest_Rate, Overdraft, Last_Access_Time_Stamp)" +
+                "VALUES (\"717\", \"102\", \"0\", \"800.14\", \"0\", \"0.00\", \"%s\")", currentTimestamp);
+        DatabaseDriver.run(sql);
+
+
+        DatabaseDriver.viewTable("'LOCAL ACCOUNT'");
+        DatabaseDriver.viewTable("Person");
+        DatabaseDriver.viewTable("Account");
+
+
+    }
+
+
+}
